dc4419f00e9eeccbf3025e905e0ee74487ee406f
Kapitan Batas <ajamiscosa@gmail.com>
2018-10-28 13:04:29 +0800

    follow up

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->increments('ID');
+            $table->string('Username');
+            $table->string('Password');
+            $table->string('EmailAddress')->unique();
+            $table->string("LastName");
+            $table->string('FirstName');
+            $table->string('MiddleName')->nullable();
+            $table->char('Gender',1)->default('M');
+            //$table->json('Roles')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2018_10_28_000000_CreateRecordsTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->increments('ID');
+            $table->string("Address");
+            $table->string("PublicKey");
+            $table->string("PrivateKey");
+            $table->unsignedInteger('created_by');
+            $table->unsignedInteger('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
